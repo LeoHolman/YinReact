23633ddeb1849612e11c80270c5dc39e34bcd98e
LeoHolman <lah2719@g.rit.edu>
2019-12-19 19:58:41 -0500

    TwoChoiceQuiz remembers answers, stops prompting after final stimuli

--- yin/src/components/Answer.js
+++ yin/src/components/Answer.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react';
 
 class Answer extends Component{
-    render(){
-        return(
-            <div id={'response-'+this.props.number} className="response">{this.chooseImage()}
-            </div>
-        )
-    }
 
     chooseImage(){
         switch(this.props.number){
             case 'one':
-                return(<img src={require('../assets/images/1_graph.svg')} alt="tone 1" />);
+                return(<img src={require('../assets/images/1_graph.svg')} alt="tone 1" title="1" />);
             case 'two':
-                return(<img src={require('../assets/images/2_graph.svg')} alt="tone 2" />);
+                return(<img src={require('../assets/images/2_graph.svg')} alt="tone 2" title="2" />);
             case 'three':
-                return(<img src={require('../assets/images/3_graph.svg')} alt="tone 3" />);
+                return(<img src={require('../assets/images/3_graph.svg')} alt="tone 3" title="3"/>);
             case 'four':
-                return(<img src={require('../assets/images/4_graph.svg')} alt="tone 4" />);
+                return(<img src={require('../assets/images/4_graph.svg')} alt="tone 4" title="4"/>);
             default:
                 return('Something went wrong');
         }
+    }
+
+    render(){
+        return(
+            <div id={'response-'+this.props.number} onClick={e => this.props.collectResponse(e)} className="response">{this.chooseImage()}
+            </div>
+        )
     }
 }
 
--- yin/src/components/TwoChoiceQuiz.js
+++ yin/src/components/TwoChoiceQuiz.js
@@ -8,21 +8,33 @@ class TwoChoiceQuiz extends Component {
             currentStimulus: 0,
         };
         this.handleClick = this.handleClick.bind(this);
+        this.collectResponse = this.collectResponse.bind(this);
     }
 
     handleClick() {
-        this.setState({currentStimulus: this.state.currentStimulus + 1});
+        this.setState({'currentStimulus': this.state.currentStimulus + 1});
+    }
+
+    collectResponse(event){
+        const questionLabel = `question${this.state.currentStimulus}`;
+        console.log(event.target.title);
+        this.setState({[questionLabel]: event.target.title});
+        this.handleClick();
     }
 
     render(){
         return(
             <div className="activity-wrap two-choice">
                 {this.props.stimuli && <div id="stimuli">{this.props.stimuli[this.state.currentStimulus]}</div>}
-                <div className = "answers">
-                    <Answer number="one" />
-                    <Answer number="two" />
-                    <button onClick={this.handleClick}>Next</button>
-                </div>
+                {this.props.stimuli && 
+                    this.props.stimuli.length > this.state.currentStimulus ? 
+                        <div className = "answers">
+                            <Answer number="one" collectResponse={this.collectResponse}/>
+                            <Answer number="two" collectResponse={this.collectResponse}/>
+                        </div>
+                        :
+                        <p>All done!</p>
+                }
             </div>
         )
     }
