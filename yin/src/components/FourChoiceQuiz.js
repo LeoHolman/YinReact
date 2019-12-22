67eadf8b53fbaeb396c65155305ece718b4e0691
Christine M <cxm1880@rit.edu>
2019-12-22 15:40:56 -0500

    Update Four Choice to take audios object

--- yin/src/components/FourChoiceQuiz.js
+++ yin/src/components/FourChoiceQuiz.js
@@ -2,17 +2,41 @@ import React, {Component} from 'react';
 import Answer from './Answer';
 
 class FourChoiceQuiz extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            currentStimulus: 0,
+        };
+        this.handleClick = this.handleClick.bind(this);
+        this.collectResponse = this.collectResponse.bind(this);
+    }
+
+    handleClick() {
+        this.setState({'currentStimulus': this.state.currentStimulus + 1});
+    }
+
+    collectResponse(event){
+        const questionLabel = `question${this.state.currentStimulus}`;
+        console.log(event.target.title);
+        this.setState({[questionLabel]: event.target.title});
+        this.handleClick();
+    }
+
     render(){
         return(
             <div className="activity-wrap four-choice">
-                <div id="stimuli">{this.props.stimuli}</div>
-                <div className = "answers">
-                    <Answer number="one" />
-                    <Answer number="two" />
-                    <Answer number="three" />
-                    <Answer number="four" />
-
-                </div>
+                {this.props.stimuli && <div id="stimuli">{this.props.stimuli[this.state.currentStimulus]}</div>}
+                {this.props.stimuli && 
+                    this.props.stimuli.length > this.state.currentStimulus ? 
+                        <div className = "answers">
+                            <Answer number="one" collectResponse={this.collectResponse}/>
+                            <Answer number="two" collectResponse={this.collectResponse}/>
+                            <Answer number="three" collectResponse={this.collectResponse}/>
+                            <Answer number="four" collectResponse={this.collectResponse}/>
+                        </div>
+                        :
+                        <p>All done!</p>
+                }
             </div>
         )
     }
