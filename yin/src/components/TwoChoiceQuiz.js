8ac8d6b2ce5301e50ce8c9368c326f7a5a8f55ab
cmccullough012 <cxm1880@rit.edu>
2019-12-29 20:15:18 -0500

    Randomize almost working

--- yin/src/components/TwoChoiceQuiz.js
+++ yin/src/components/TwoChoiceQuiz.js
@@ -11,9 +11,12 @@ class TwoChoiceQuiz extends Component {
         this.handleClick = this.handleClick.bind(this);
         this.collectResponse = this.collectResponse.bind(this);
         this.nextQuestion = this.nextQuestion.bind(this);
-        this.componentDidMount  = this.componentDidMount.bind(this);
+        // this.componentDidMount  = this.componentDidMount.bind(this);
         this.randomTone  = this.randomTone.bind(this);
+        this.randomize = this.randomize.bind(this);
     }
+
+
 
     // static getStateDerivedFromProps(props, state){
     //     return {stimuli: props.stimuli};
@@ -28,38 +31,69 @@ class TwoChoiceQuiz extends Component {
         this.setState({[questionLabel]: event.target.title});
     }
 
-   randomTone(stimuli){
-        var optionArr =[];
-        var word = [];
-        if(stimuli[this.state.currentStimulus]){
-            word.push(stimuli[this.state.currentStimulus].word);
-            optionArr.push(word);
-            word=[];
-            for (var i=0; i<2; i++){
-                word.push(stimuli[this.state.currentStimulus].alternateTones[i]);
-                optionArr.push(word);
-                word=[];
-            }
-            // this.setState({'options': optionArr});
-            // console.log('in random' + stimuli[this.state.currentStimulus].word);
-
+    randomize(a){
+        var j, x, i;
+        for (i = a.length - 1; i > 0; i--) {
+            j = Math.floor(Math.random() * (i + 1));
+            x = a[i];
+            a[i] = a[j];
+            a[j] = x;
         }
-        return "three";
+        return a;
     }
 
-    async componentDidMount(){
-        // await this.props.stimuli; 
-        // this.randomTone();
-        // if (this.props.stimuli){
-        //     this.randomTone();
+    randomTone(stimuli, num){
+        var optionArr =[];
+        var randomArr = [];
+        var word = [];
+        // var count = this.state.optionCount;
+        // this.setState({'optionCount': this.state.optionCount++});
+        //randomize the alternate tones 
+        randomArr = this.randomize(stimuli[this.state.currentStimulus].alternateTones);
+        for (var i=0; i<randomArr.length; i++){
+            if (randomArr[i] == this.props.stimuli.correctTone){
+                word.push(randomArr[i]);
+                word.push(randomArr[i].charAt(randomArr[i].length-1));
+                optionArr.push(word);
+                randomArr.splice(i, 1); 
+            }
+        }
+        word.push(randomArr[0]);
+        word.push(randomArr[0].charAt(randomArr[0].length-1));
+        optionArr.push(word);
+
+        console.log(this.state.options);
+        // if(stimuli[this.state.currentStimulus]){
+        //     word.push(stimuli[this.state.currentStimulus].word);
+        //     optionArr.push(word);
+        //     word=[];
+        //     for (var i=0; i<2; i++){
+        //         word.push(stimuli[this.state.currentStimulus].alternateTones[i]);
+        //         optionArr.push(word);
+        //         word=[];
+        //     }
+        //     // this.setState({'options': optionArr});
+        //     // console.log('in random' + stimuli[this.state.currentStimulus].word);
+
         // }
-        // this.setState({'stimuli': this.props.stimuli})
-        // .then( () => {
-        //     // this.randomTone();
-        //     console.log(this.state.options);
-        // });
-
+        return optionArr[0];
     }
+
+
+
+    // async componentDidMount(){
+    //     // await this.props.stimuli; 
+    //     // this.randomTone();
+    //     // if (this.props.stimuli){
+    //     //     this.randomTone();
+    //     // }
+    //     // this.setState({'stimuli': this.props.stimuli})
+    //     // .then( () => {
+    //     //     // this.randomTone();
+    //     //     console.log(this.state.options);
+    //     // });
+
+    // }
 
     
 
@@ -76,8 +110,8 @@ class TwoChoiceQuiz extends Component {
                         <>
                         <div className="stimuli">word: {this.props.stimuli[this.state.currentStimulus].word}</div>
                         <div className = "answers">
-                            <Answer number={this.randomTone(this.props.stimuli)} collectResponse={this.collectResponse} />
-                            <Answer number="two" collectResponse={this.collectResponse}/>
+                            <Answer number={this.randomTone(this.props.stimuli, 0)} collectResponse={this.collectResponse} />
+                            <Answer number={this.randomTone(this.props.stimuli, 1)} collectResponse={this.collectResponse}/>
                         </div>
                         <button onClick={this.handleClick}>Next</button>
                         </>
