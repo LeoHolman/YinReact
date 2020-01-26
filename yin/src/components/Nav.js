fdb2163837eaa2a925f9939daf80f2b74aa20333
cmccullough012 <cxm1880@rit.edu>
2020-01-26 14:25:54 -0500

    Styling quiz with 2+ characters

--- yin/src/components/Nav.js
+++ yin/src/components/Nav.js
@@ -10,8 +10,8 @@ class Nav extends Component {
                     <li><a href="https://yin.rit.edu">Return to Yin </a></li>
                     <li><Link to="/lessons/">Lessons & Quizzes &emsp;|</Link></li>
                     <li><Link to="/login/">Login &emsp;|</Link> </li>
-                    {this.props.is_teacher &&
-                        <li><Link to="/teacherInterface/">Teachers &emsp;|</Link></li>}
+                    {/* {this.props.is_teacher &&
+                        <li><Link to="/teacherInterface/">Teachers &emsp;|</Link></li>} */}
                 </ul>
             </nav>
         )
