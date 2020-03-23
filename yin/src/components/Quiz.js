96e6f80ad6aec59fb0a4e9f912d9b293faac0acd
Christine M <cxm1880@rit.edu>
2020-03-23 15:34:25 -0400

    Mobile navigation

--- yin/src/components/Quiz.js
+++ yin/src/components/Quiz.js
@@ -6,7 +6,7 @@ import Production from './Production';
 class Quiz extends Component{
     constructor(props){
         super(props);
-        console.log(props);
+        // console.log(props);
         this.state = {
             activity1:{
                 score: null,
