fbd4520d0173b1d035f24bff75bf569424c7a37f
Leo Holman <lah2719@g.rit.edu>
2021-05-04 14:40:36 -0400

    Refactor teacher nav to functional component

--- yin/src/components/TeacherNav.js
+++ yin/src/components/TeacherNav.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import '../css/teacherInterface.css';
 
-class TeacherNav extends Component {
-    render(){
-        return(
-            <div id="teacher-nav">
-            {this.props.view === "lessons" ? 
-                <>
-                    <Link to={'/teacherInterface/lessons/add'} id="addLessonLink">
-                            + &emsp;Add a Lesson
-                    </Link>
-                    <h1>Lessons | <Link to={'/teacherInterface/words'}>Words </Link></h1>
-                </>
-            :
-                <>
-                    <Link to={'/teacherInterface/words/add'} id="addLessonLink">
-                            + &emsp;Add a Word
-                    </Link>
-                    <h1><Link to={'/teacherInterface/lessons'}>Lessons</Link> | Words</h1>
-                </>
-            }
-            
-            </div>
-
-        )
-    }
+const TeacherNav = ({view}) => {
+    return(
+        <div id="teacher-nav">
+        {view === "lessons" ? 
+            <>
+                <Link to={'/teacherInterface/lessons/add'} id="addLessonLink">
+                        + &emsp;Add a Lesson
+                </Link>
+                <h1>Lessons | <Link to={'/teacherInterface/words'}>Words </Link></h1>
+            </>
+        :
+            <>
+                <Link to={'/teacherInterface/words/add'} id="addLessonLink">
+                        + &emsp;Add a Word
+                </Link>
+                <h1><Link to={'/teacherInterface/lessons'}>Lessons</Link> | Words</h1>
+            </>
+        }
+        </div>
+    )
 }
 
 export default TeacherNav;
