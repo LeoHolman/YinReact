588431c2b10039c83adb39c23ed9e0e90a6f30a0
Leo Holman <lah2719@g.rit.edu>
2021-05-06 09:07:15 -0400

    Update to wad module for recorder, currently not properly connecting to output function

--- yin/src/components/Recorder.js
+++ yin/src/components/Recorder.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Wad from 'web-audio-daw';
 
 const Recorder = ({outputFunction, label}) => {
     // const [recording, setRecording] = useState(
@@ -11,73 +12,104 @@ const Recorder = ({outputFunction, label}) => {
         recordButton.classList.remove('green');
     }, [])
 
-    async function addRecordFunction() {
-        record("__record_button")
-            .then( blob => {
-                processAudio(blob)
-                    .then( async (csvDataLocation) => {
-                        const start = csvDataLocation.search(/(\*\*\*)/)+5
-                        const end = csvDataLocation.search('&&&')
-                        var url = csvDataLocation.substring(start,end)
-                        url = 'https://yin.rit.edu/' + url;
-                        const data = await getDataSet(url);
-                        outputFunction(data);
-                    })	
-            })
+    var voice = new Wad({source: 'mic'});
+    var tuner = new Wad.Poly();
+    var myReq;
+    var count = 0;
+    tuner.setVolume(0);
+    tuner.add(voice);
+    function call_pitch(count){
+            voice.play();
+            tuner.updatePitch();
+            setButtonClass('red');
+            var logPitch = function(){
+                console.log(tuner.pitch)
+                myReq = requestAnimationFrame(logPitch)
+            }
+            logPitch();
+    }
+    function stop_pitch(){
+        tuner.stopUpdatingPitch();
     }
 
-    async function getDataSet(url){
-        const data = await (await fetch(`${url}`)).text();
-        return data;
+    function addRecordFunction(){
+        myReq = call_pitch();
+        setTimeout(() => {
+            console.log('Stop pitch called')
+            tuner.stopUpdatingPitch();
+            cancelAnimationFrame(myReq)
+            setButtonClass('green');
+        }, 2000);
     }
 
-    function record(buttonId) {
-        return new Promise(resolve => {
-            // var audio;
-            // var audioUrl;
-            var audioBlob;
-            // const recordButton = document.getElementById(buttonId);
-            navigator.mediaDevices.getUserMedia({
-                    audio: true
-                })
-                .then(stream => {
-                    const mediaRecorder = new MediaRecorder(stream);
-                    mediaRecorder.start();
-                    setButtonClass('red');
-    
-                    const audioChunks = [];
-    
-                    mediaRecorder.addEventListener("dataavailable", event => {
-                        audioChunks.push(event.data);
-                    });
-    
-                    mediaRecorder.addEventListener("stop", () => {
-                        audioBlob = new Blob(audioChunks, {
-                            type: 'audio/wav; codecs=MS_PCM'
-                        });
-                        resolve(audioBlob);
-                    });
-    
-                    setTimeout(() => {
-                        mediaRecorder.stop();
-                        setButtonClass('green');
-                    }, 2000);
-                });
-        })
-    };
 
-    async function processAudio(audioBlob) {
-        return new Promise(resolve => {
-            // var rawResponse;
-            // var csvDataLocation;
-            var formData = new FormData();
-            formData.append("audioData", audioBlob);
-            fetch('https://yin.rit.edu/pages/audioProcessing.php', {
-                method: 'POST', 
-                body: formData,
-            }).then((res) => resolve(res.text()));
-        });
-    }
+    // async function addRecordFunction() {
+    //     record("__record_button")
+    //         .then( blob => {
+    //             processAudio(blob)
+    //                 .then( async (csvDataLocation) => {
+    //                     const start = csvDataLocation.search(/(\*\*\*)/)+5
+    //                     const end = csvDataLocation.search('&&&')
+    //                     var url = csvDataLocation.substring(start,end)
+    //                     url = 'https://yin.rit.edu/' + url;
+    //                     const data = await getDataSet(url);
+    //                     outputFunction(data);
+    //                 })	
+    //         })
+    // }
+
+    // async function getDataSet(url){
+    //     const data = await (await fetch(`${url}`)).text();
+    //     return data;
+    // }
+
+    // function record(buttonId) {
+    //     return new Promise(resolve => {
+    //         // var audio;
+    //         // var audioUrl;
+    //         var audioBlob;
+    //         // const recordButton = document.getElementById(buttonId);
+    //         navigator.mediaDevices.getUserMedia({
+    //                 audio: true
+    //             })
+    //             .then(stream => {
+    //                 const mediaRecorder = new MediaRecorder(stream);
+    //                 mediaRecorder.start();
+    //                 setButtonClass('red');
+    
+    //                 const audioChunks = [];
+    
+    //                 mediaRecorder.addEventListener("dataavailable", event => {
+    //                     audioChunks.push(event.data);
+    //                 });
+    
+    //                 mediaRecorder.addEventListener("stop", () => {
+    //                     audioBlob = new Blob(audioChunks, {
+    //                         type: 'audio/wav; codecs=MS_PCM'
+    //                     });
+    //                     resolve(audioBlob);
+    //                 });
+    
+    //                 setTimeout(() => {
+    //                     mediaRecorder.stop();
+    //                     setButtonClass('green');
+    //                 }, 2000);
+    //             });
+    //     })
+    // };
+
+    // async function processAudio(audioBlob) {
+    //     return new Promise(resolve => {
+    //         // var rawResponse;
+    //         // var csvDataLocation;
+    //         var formData = new FormData();
+    //         formData.append("audioData", audioBlob);
+    //         fetch('https://yin.rit.edu/pages/audioProcessing.php', {
+    //             method: 'POST', 
+    //             body: formData,
+    //         }).then((res) => resolve(res.text()));
+    //     });
+    // }
 
     return(
         <>
