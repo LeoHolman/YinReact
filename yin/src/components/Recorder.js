d964eb9c355adeb84e4f03e4f6f61c018d63a090
Leo Holman <leo.a.holman@gmail.com>
2021-08-02 18:10:20 -0400

    refactor: async/await

--- yin/src/components/Recorder.js
+++ yin/src/components/Recorder.js
@@ -7,66 +7,62 @@ function Recorder({ label, outputFunction }) {
   const [buttonClass, setButtonClass] = useState("");
 
   async function processAudio(audioBlob) {
-    return new Promise((resolve) => {
+    return new Promise(async (resolve) => {
       const formData = new FormData();
       formData.append("file", audioBlob, "recorder.wav");
-      fetch("http://localhost:8080/extract_pitch/wav", {
+      const result = await fetch("http://localhost:8080/extract_pitch/wav", {
         method: "POST",
         body: formData,
-      }).then((res) => {
-        resolve(res.text());
       });
+      resolve(result.text());
     });
   }
 
-  function record() {
-    return new Promise((resolve) => {
+  async function record() {
+    return new Promise(async (resolve) => {
       let audioBlob;
       let mediaRecorder;
-      navigator.mediaDevices
-        .getUserMedia({
-          audio: true,
-          video: false,
-        })
-        .then((stream) => {
-          mediaRecorder = new MediaRecorder(stream, { mimeType: "audio/wav" });
-          mediaRecorder.start();
-          setButtonClass("red");
-          const audioChunks = [];
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
 
-          mediaRecorder.addEventListener("dataavailable", (event) => {
-            audioChunks.push(event.data);
-          });
+      mediaRecorder = new MediaRecorder(stream, { mimeType: "audio/wav" });
+      mediaRecorder.start();
+      setButtonClass("red");
+      const audioChunks = [];
 
-          mediaRecorder.addEventListener("stop", () => {
-            audioBlob = new Blob(audioChunks, {
-              type: "audio/wav",
-            });
-            resolve(audioBlob);
-          });
+      mediaRecorder.addEventListener("dataavailable", (event) => {
+        audioChunks.push(event.data);
+      });
 
-          setTimeout(() => {
-            mediaRecorder.stop();
-            mediaRecorder = undefined;
-            setButtonClass("green");
-          }, 2000);
+      mediaRecorder.addEventListener("stop", () => {
+        audioBlob = new Blob(audioChunks, {
+          type: "audio/wav",
         });
+        resolve(audioBlob);
+      });
+
+      setTimeout(() => {
+        mediaRecorder.stop();
+        mediaRecorder = undefined;
+        setButtonClass("green");
+      }, 2000);
     });
   }
 
   async function addRecordFunction() {
     await registerMediaRecorder();
-    record("__record_button").then((blob) => {
-      processAudio(blob).then(async (data) => {
-        // Remove useless header information
-        const lines = data.split("\n");
-        lines.splice(0, 3);
-        // Add useful column labels
-        lines.unshift(["time\tfrequency"]);
-        const splicedData = lines.join("\n");
-        outputFunction(splicedData);
-      });
-    });
+    const blob = await record("__record_button");
+    const data = await processAudio(blob);
+
+    // Remove useless header information
+    const lines = data.split("\n");
+    lines.splice(0, 3);
+    // Add useful column labels
+    lines.unshift(["time\tfrequency"]);
+    const splicedData = lines.join("\n");
+    outputFunction(splicedData);
   }
 
   return (
